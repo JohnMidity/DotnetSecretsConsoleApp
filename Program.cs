8e37301de5d5f06eaee0097ea387c5cf7a1a20ba
JohnMidity <john.zandbergen@gmail.com>
2023-08-18 15:06:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetSecretsConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dotnet Secrets Console App example");
+        }
+    }
+}
